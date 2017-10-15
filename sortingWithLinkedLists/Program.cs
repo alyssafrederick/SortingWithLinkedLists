5a7452afc894602b1cd29d5c80280a5a72a7fb30
alyssafrederick <peaceloveyummydance@gmail.com>
2017-10-15 12:32:25 -0700

    made the simple sorts generic and in a class. also started merge sort

--- sortingWithLinkedLists/Program.cs
+++ sortingWithLinkedLists/Program.cs
@@ -24,95 +24,15 @@ namespace sortingWithLinkedLists
             }
 
 
-            ////////////////////////////////////bubble sort////////////////////
-            bool switched;
-
-            //do
-            //{
-            //    switched = false;
-
-            //    for (int i = 0; i < numlist.Size - 1; i++)
-            //    {
-
-            //        if (numlist[i] > numlist[i + 1])
-            //        {
-            //            int temp = numlist[i];
-            //            numlist[i] = numlist[i + 1];
-            //            numlist[i + 1] = temp;
-
-            //            switched = true;
-            //        }
-            //    }
-
-            //} while (switched == true);
-
-            //Console.Write("\n \n");
-
-            //for (int k = 0; k < numlist.Size; k++)
-            //{
-            //    Console.WriteLine("{0}", numlist[k]);
-            //}
 
 
+            Sort<int> sortthis = new Sort<int>();
 
+            //sortthis.BubbleSort(numlist);
+            //sortthis.SelectionSort(numlist);
+            //sortthis.InsertionSort(numlist);
 
-
-            /////////////////////////////////////selection sort//////////////////
-            //int index2swap;
-            //int whereWeAre;
-
-            //for (int i = 0; i < numlist.Size; i++)
-            //{
-            //    index2swap = i;
-            //    whereWeAre = numlist[i];
-
-            //    for (int k = i + 1; k < numlist.Size; k++)
-            //    {
-            //        if (whereWeAre > numlist[k])
-            //        {
-            //            index2swap = k;
-            //            whereWeAre = numlist[k];
-            //        }
-            //    }
-
-            //    int temp = numlist[i];
-            //    numlist[i] = numlist[index2swap];
-            //    numlist[index2swap] = temp;
-            //}
-
-            //Console.Write("\n \n");
-
-            //for (int j = 0; j < numlist.Size; j++)
-            //{
-            //    Console.WriteLine("{0}", numlist[j]);
-            //}
-
-
-
-
-
-            ////////////////////////////////////////////insertion sort////////////
-            for (int i = 0; i < numlist.Size; i++)
-            {
-                for (int a = i - 1; a > -1; a--)
-                {
-                    if (numlist[a] > numlist[a + 1])
-                    {
-                        int temp = numlist[a + 1];
-                        numlist[a + 1] = numlist[a];
-                        numlist[a] = temp;
-                    }
-                }
-            }
-
-
-
-            Console.Write("\n \n");
-
-            for (int j = 0; j < numlist.Size; j++)
-            {
-                Console.WriteLine("{0}", numlist[j]);
-            }
+            sortthis.MergeSort(numlist);
 
 
             Console.ReadKey();
---
+++ sortingWithLinkedLists/Sort.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sortingWithLinkedLists
+{
+    class Sort<T> where T : IComparable
+    {
+        public void BubbleSort(LinkedList<T> tosort)
+        {
+            bool switched;
+
+            do
+            {
+                switched = false;
+
+                for (int i = 0; i < tosort.Size - 1; i++)
+                {
+
+                    if (tosort[i].CompareTo(tosort[i + 1]) > 0)
+                    {
+                        T temp = tosort[i];
+                        tosort[i] = tosort[i + 1];
+                        tosort[i + 1] = temp;
+
+                        switched = true;
+                    }
+                }
+
+            } while (switched == true);
+
+            Console.Write("\n \n");
+
+            for (int k = 0; k < tosort.Size; k++)
+            {
+                Console.WriteLine("{0}", tosort[k]);
+            }
+        }
+
+        public void SelectionSort(LinkedList<T> tosort)
+        {
+            int index2swap;
+            T whereWeAre;
+
+            for (int i = 0; i < tosort.Size; i++)
+            {
+                index2swap = i;
+                whereWeAre = tosort[i];
+
+                for (int k = i + 1; k < tosort.Size; k++)
+                {
+                    if (whereWeAre.CompareTo(tosort[k]) > 0)
+                    {
+                        index2swap = k;
+                        whereWeAre = tosort[k];
+                    }
+                }
+
+                T temp = tosort[i];
+                tosort[i] = tosort[index2swap];
+                tosort[index2swap] = temp;
+            }
+
+            Console.Write("\n \n");
+
+            for (int j = 0; j < tosort.Size; j++)
+            {
+                Console.WriteLine("{0}", tosort[j]);
+            }
+        }
+
+        public void InsertionSort(LinkedList<T> tosort)
+        {
+            for (int i = 0; i < tosort.Size; i++)
+            {
+                for (int a = i - 1; a > -1; a--)
+                {
+                    if (tosort[a].CompareTo(tosort[a + 1]) > 0)
+                    {
+                        T temp = tosort[a + 1];
+                        tosort[a + 1] = tosort[a];
+                        tosort[a] = temp;
+                    }
+                }
+            }
+
+            Console.Write("\n \n");
+
+            for (int j = 0; j < tosort.Size; j++)
+            {
+                Console.WriteLine("{0}", tosort[j]);
+            }
+        }
+
+        public void mergesort(LinkedList<T> tosort, int start, int end)
+        {
+            if (start <= end)
+            {
+                int middle = (start + end) / 2;
+                mergesort(tosort, start, middle);
+                mergesort(tosort, middle + 1, end);
+                merge(tosort, start, middle, end);
+            }
+        }
+
+        public void merge(LinkedList<T> tosort, int start, int middle, int end)
+        {
+            LinkedList<T> leftlist = new LinkedList<T>();
+            LinkedList<T> rightlist = new LinkedList<T>();
+
+            if (leftlist.First.Value.CompareTo(leftlist.) > 0)
+            {
+
+            }
+        }
+
+        public void MergeSort(LinkedList<T> tosort)
+        {
+            mergesort(tosort, 0, tosort.Size - 1);
+
+            Console.Write("\n \n");
+
+            for (int j = 0; j < tosort.Size; j++)
+            {
+                Console.WriteLine("{0}", tosort[j]);
+            }
+        }
+
+    }
+}
